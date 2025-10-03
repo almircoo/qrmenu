2dddeaa5472b0c0ef180efdd2a289acb1b65ad3d
almircoo <almircoo@proton.me>
2025-10-03 22:40:42 +0100

    register authcontext for validate routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+import { AuthContext } from '@contexts/AuthContext'
+import App from '@routes/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthContext>
+        <App />
+      </AuthContext>
+    </BrowserRouter>
   </StrictMode>,
 )
