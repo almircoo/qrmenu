d0285b28065cb50fa51ed298939b1bb17c6d842c
almircoo <almircoo@proton.me>
2025-10-05 21:27:46 +0100

    change browser registert to app components

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router'
-import { AuthProvider } from '@contexts/AuthContext'
-import App from '@routes/App'
+import { App } from './routes/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App/>
-      </AuthProvider>
-    </BrowserRouter>
+    <App/>
   </StrictMode>,
 )
