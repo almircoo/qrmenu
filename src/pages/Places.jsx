c4e9fb51556a1b1f399e081825460716059ae502
almircoo <almircoo@proton.me>
2025-10-06 19:27:56 +0100

    create places page

---
+++ src/pages/Places.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MainLayout } from '@/layouts/MainLayout'
+export const Places = () => {
+  return (
+    <MainLayout>
+      <div>Places</div>
+    </MainLayout>
+    
+  )
+}
