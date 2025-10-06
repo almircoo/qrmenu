b6d7d1e198074a0fb8e45a8ef60fd7a4151b97b7
almircoo <almircoo@proton.me>
2025-10-06 17:45:24 +0100

    add login and register routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { Routes, Route, BrowserRouter } from "react-router"
 import { Home } from '../pages/Home'
+import { Login } from '@/pages/Login'
+import { Register } from '@/pages/Register'
 export const App = () => {
   return (
     <>
@@ -10,6 +12,8 @@ export const App = () => {
           <Routes>
             {/* Rutas publicas */}
             <Route path='/' element={<Home/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
